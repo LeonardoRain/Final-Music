c20844c92c74b6d6477f36d9f006b5d7fdec304a
LeonardoRain <295497522@qq.com>
2022-02-23 16:18:03 +0800

    完成推荐页轮播图组件

--- final-music/src/views/recommend.vue
+++ final-music/src/views/recommend.vue
@@ -1,12 +1,53 @@
 <template>
-  <div class="recommend">推荐页面</div>
+  <div class="recommend">
+    <div class="slider-wrapper">
+      <div class="slider-content">
+        <slider v-if="sliders.length" :sliders="sliders"></slider>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { getRecommend } from "@/service/recommend";
+import Slider from "@/components/base/slider/slider";
+
 export default {
   name: "recommend",
-  components: {},
+  components: {
+    Slider,
+  },
+  data() {
+    return {
+      sliders: [],
+    };
+  },
+  async created() {
+    const result = await getRecommend();
+    this.sliders = result.sliders;
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.recommend {
+	position: fixed;
+	top: 88px;
+	bottom: 0;
+	width: 100%;
+	.slider-wrapper {
+		overflow: hidden;
+		position: relative;
+		padding-top: 40%;
+		width: 100%;
+		height: 0;
+		.slider-content {
+			position: absolute;
+			left: 0;
+			top: 0;
+			width: 100%;
+			height: 100%;
+		}
+	}
+}
+</style>
--- final-music/vue.config.js
+++ final-music/vue.config.js
@@ -1,4 +1,4 @@
-// const registerRouter = require("./backend/router");
+const registerRouter = require("./backend/router");
 
 module.exports = {
   css: {
@@ -12,11 +12,11 @@ module.exports = {
       },
     },
   },
-  // devServer: {
-  //   before(app) {
-  //     registerRouter(app);
-  //   },
-  // },
+  devServer: {
+    before(app) {
+      registerRouter(app);
+    },
+  },
   configureWebpack: (config) => {
     if (process.env.npm_config_report) {
       const BundleAnalyzerPlugin =
