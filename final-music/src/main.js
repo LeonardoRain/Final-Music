a5d46931df1e3387c8fcdc347d865079b680cd4a
LeonardoRain <295497522@qq.com>
2022-02-24 14:46:02 +0800

    完成自定义指令 v-loading

--- final-music/src/main.js
+++ final-music/src/main.js
@@ -2,8 +2,17 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import lazyPlugin from "vue3-lazy";
+import loadingDirective from "@/components/base/loading/directive";
 
 // 全局引入样式文件
 import "@/assets/scss/index.scss";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(lazyPlugin, {
+    loading: require("@/assets/images/default.png"),
+  })
+  .directive("loading", loadingDirective)
+  .mount("#app");
--- final-music/src/views/recommend.vue
+++ final-music/src/views/recommend.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="recommend">
+  <div class="recommend" v-loading:[loadingText]="loading">
     <scroll class="recommend-content">
       <div>
         <div class="slider-wrapper">
@@ -8,11 +8,11 @@
           </div>
         </div>
         <div class="recommend-list">
-          <h1 class="list-title">热门歌单推荐</h1>
+          <h1 class="list-title" v-show="!loading">热门歌单推荐</h1>
           <ul>
             <li v-for="item in albums" class="item" :key="item.id">
               <div class="icon">
-                <img width="60" height="60" :src="item.pic" alt="" />
+                <img width="60" height="60" v-lazy="item.pic" alt="" />
               </div>
               <div class="text">
                 <p class="title">{{ item.title }}</p>
@@ -41,7 +41,13 @@ export default {
     return {
       sliders: [],
       albums: [],
+      loadingText: "正在载入...",
     };
+  },
+  computed: {
+    loading() {
+      return !this.sliders.length && !this.albums.length;
+    },
   },
   async created() {
     const result = await getRecommend();
