bfe61692d7c854f1f4af5089a8389ba4c343f1c9
LeonardoRain <295497522@qq.com>
2023-03-03 14:30:35 +0800

    update

--- final-music/src/components/music-list/music-list.vue
+++ final-music/src/components/music-list/music-list.vue
@@ -153,88 +153,88 @@ export default {
 
 <style lang="scss" scoped>
 .music-list {
-	position: relative;
-	height: 100%;
-	.back {
-		position: absolute;
-		left: 6px;
-		top: 0;
-		z-index: 20;
-		margin-top: 10px;
-		transform: translateZ(2px);
-		.icon-back {
-			display: block;
-			padding: 10px;
-			font-size: $font-size-large-x;
-			color: $color-theme;
-		}
-	}
-	.title {
-		position: absolute;
-		left: 10%;
-		top: 0;
-		z-index: 20;
-		margin-top: 10px;
-		width: 80%;
-		line-height: 40px;
-		text-align: center;
-		font-size: $font-size-large;
-		color: $color-theme;
-		transform: translateZ(2px);
+  position: relative;
+  height: 100%;
+  .back {
+    position: absolute;
+    left: 6px;
+    top: 0;
+    z-index: 20;
+    margin-top: 10px;
+    transform: translateZ(2px);
+    .icon-back {
+      display: block;
+      padding: 10px;
+      font-size: $font-size-large-x;
+      color: $color-theme;
+    }
+  }
+  .title {
+    position: absolute;
+    left: 10%;
+    top: 0;
+    z-index: 20;
+    margin-top: 10px;
+    width: 80%;
+    line-height: 40px;
+    text-align: center;
+    font-size: $font-size-large;
+    color: $color-theme;
+    transform: translateZ(2px);
 
-		@include no-wrap();
-	}
-	.bg-image {
-		position: relative;
-		width: 100%;
-		background-size: cover;
-		transform-origin: top;
-		.play-btn-wrapper {
-			position: absolute;
-			bottom: 20px;
-			z-index: 10;
-			width: 100%;
-			.play-btn {
-				box-sizing: border-box;
-				margin: 0 auto;
-				padding: 7px 0;
-				border: 1px solid $color-theme;
-				border-radius: 100px;
-				width: 135px;
-				text-align: center;
-				font-size: 0;
-				color: $color-theme;
-			}
-			.icon-play {
-				display: inline-block;
-				margin-right: 6px;
-				vertical-align: middle;
-				font-size: $font-size-medium-x;
-			}
-			.text {
-				display: inline-block;
-				vertical-align: middle;
-				font-size: $font-size-small;
-			}
-		}
-		.filter {
-			position: absolute;
-			left: 0;
-			top: 0;
-			width: 100%;
-			height: 100%;
-			background: rgba(7, 17, 27, .4);
-		}
-	}
-	.list {
-		position: absolute;
-		bottom: 0;
-		z-index: 0;
-		width: 100%;
-		.song-list-wrapper {
-			padding: 20px 30px;
-			background: $color-background;
-		}
-	}
+    @include no-wrap();
+  }
+  .bg-image {
+    position: relative;
+    width: 100%;
+    background-size: cover;
+    transform-origin: top;
+    .play-btn-wrapper {
+      position: absolute;
+      bottom: 20px;
+      z-index: 10;
+      width: 100%;
+      .play-btn {
+        box-sizing: border-box;
+        margin: 0 auto;
+        padding: 7px 0;
+        border: 1px solid $color-theme;
+        border-radius: 100px;
+        width: 135px;
+        text-align: center;
+        font-size: 0;
+        color: $color-theme;
+      }
+      .icon-play {
+        display: inline-block;
+        margin-right: 6px;
+        vertical-align: middle;
+        font-size: $font-size-medium-x;
+      }
+      .text {
+        display: inline-block;
+        vertical-align: middle;
+        font-size: $font-size-small;
+      }
+    }
+    .filter {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      background: rgba(7, 17, 27, 0.4);
+    }
+  }
+  .list {
+    position: absolute;
+    bottom: 0;
+    z-index: 0;
+    width: 100%;
+    .song-list-wrapper {
+      padding: 20px 30px;
+      background: $color-background;
+    }
+  }
 }
 </style>
